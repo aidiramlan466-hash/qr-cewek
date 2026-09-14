aca913719122e5b8d61aa86712cf274a4b4657c7
aidiramlan466-hash <aidiramlan466@gmail.com>
2026-09-14 12:44:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const button = document.getElementById("openMessage");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+    message.classList.add("show");
+    button.style.display = "none";
+});
+
+function createHeart() {
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (4 + Math.random() * 4) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 8000);
+}
+
+setInterval(createHeart, 700);
